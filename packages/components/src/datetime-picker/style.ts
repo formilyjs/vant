9448eebdb30bc4354dda090b01da431a7a92da9f
hubert <hubert@lejian.com>
2022-10-17 20:26:37 +0800

    fix: babel import 样式引用问题

--- packages/components/src/datetime-picker/style.ts
+++ packages/components/src/datetime-picker/style.ts
@@ -1,4 +1,5 @@
-import 'vant/lib/datetime-picker/index.less'
+// TODO: 样式文件不存在，待确认
+// import 'vant/lib/datetime-picker/index.less'
 
 // 依赖
 import '../preview-text/style'
--- packages/components/src/submit/style.ts
+++ packages/components/src/submit/style.ts
@@ -1,1 +1,2 @@
-import 'vant/lib/field/button.less'
+// TODO: 样式文件不存在，待确认
+// import 'vant/lib/field/button.less'
